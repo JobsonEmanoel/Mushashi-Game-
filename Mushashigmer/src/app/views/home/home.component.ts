2cf3a742812c2b475cd80f27a8b765bb19023acf
JobsonEmanoel <94983068+JobsonEmanoel@users.noreply.github.com>
2022-04-10 19:09:14 -0300

    Mushashi Game

---
+++ Mushashigmer/src/app/views/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { NoticiasService } from 'src/app/services/noticias.service';
+import { Noticias } from 'src/app/models/noticias';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private noticiaService: NoticiasService) { }
+
+  ListaNoticias = [] as Noticias[]
+
+  ngOnInit(): void {
+    this.carregarNoticias()
+  }
+
+  carregarNoticias() {
+    this.noticiaService.getNoticias().subscribe( (noticiasRecebidas: Noticias[]) => {
+      this.ListaNoticias = noticiasRecebidas;
+      console.log(this.ListaNoticias);
+    } )
+  }
+
+}
